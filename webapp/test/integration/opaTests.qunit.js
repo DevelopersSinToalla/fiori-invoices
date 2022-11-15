cae1d9c6efe3722ef1098df116cc53372115ac13
DevelopersSinToalla <developers.sintoalla@gmail.com>
2022-11-15 09:48:07 +0000

    first commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["trialdevs/invoices/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"trialdevs/invoices/test/unit/controller/MainView.controller"
+], function () {
+	"use strict";
+});
